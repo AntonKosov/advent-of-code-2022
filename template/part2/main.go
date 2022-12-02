fc51e2bdeb8992ebca450b550f02e95f40009e65
Anton Kosov <AntonRKosov@gmail.com>
2022-12-01 20:00:09 -0600

    template

---
+++ template/part2/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AntonKosov/advent-of-code-2022/aoc"
+)
+
+func main() {
+	data := read()
+	r := process(data)
+	fmt.Printf("Answer: %v\n", r)
+}
+
+func read() []string {
+	lines := aoc.ReadAllInput()
+
+	return lines
+}
+
+func process(data []string) int {
+	return 0
+}
